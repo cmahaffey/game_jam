29d58a4d46757d29c5e07642bd179dc38710170f
nilocy <mahaffec@gmail.com>
2015-08-22 15:20:21 -0400

    adds abilities and a cycling battle function. comp can't use abilities, needs functions for input.

--- public/js/abilities.js
+++ public/js/abilities.js
@@ -1,25 +1,32 @@
 function hurt(player, enemy){
-  enemy.hp-= player.imn*2;
+  enemy.hp-= player.imn*1.5;
 
 }
 
-function fortyWinks(player){
+function heal(player){
   player.hp+= player.imn*1.5;
+}
+
+function fortyWinks(player,enemy){
+  console.log(enemy.name + "has fallen asleep!")
+  enemy.deBuff='sleep';
+  enemy.deBTurns=Math.floor((Math.rand()*3)+1);
+}
+
+function beBrave(player,enemy){
+  player.hp+= player.imn*1.5;
+  player.buff='brave';
+  player.buffTurns=1;
+}
+
+function tantrum(player,enemy){
+  enemy.hp-= player.imn*4;
 }
 
 // function hurtMore(player,enemy){
 //   enemy.hp-= player.imn*4;
 // }
 
-function tantrum(player,enemy){
-  enemy.hp-= player.imn*2;
-}
-
 // function eightyWinks(player){
 //   player.hp+= player.imn*3;
 // }
-
-function beBrave(player,enemy){
-  enemy.hp-= player.imn*2;
-  player.hp+= player.imn*1.5;
-}
--- public/js/character.js
+++ public/js/character.js
@@ -1,21 +1,52 @@
 function Character(options){
   this.name=(options.name)
   this.lvl=(options.lvl),
-  this.hp=(5*options.def),
-  this.mp=(3*options.imn),
+  this.maxHp=(5*options.def),
+  this.maxMp=(3*options.imn),
+  this.hp=this.maxHp,
+  this.mp=this.maxMp,
   this.att=options.att,
   this.def=options.def,
   this.imn=options.imn,
   this.spd=options.spd,
   this.inventory=options.inventory || [],
   this.abilities=options.abilities || [],
-  this.equipment=options.equipment || [],
+  this.weapon=options.weapon|| null,
+  this.equipment=options.equipment || null,
   this.player= options.play || false,
   this.exp=0,
-  this.expMax=lvl*10;
+  this.expMax=lvl*10,
+  this.deBuff=null,
+  this.buff=null,
+  this.turn=0,
+  this.buffTurns=0,
+  this.deBTurns=0
+}
+
+Character.prototype.battle=function battle(enemy){
+  this.Turn++;
+  if (this.buffTurns>1){
+    this.buffTurns--;
+  }else if (this.buffTurns===1) {
+    this.buffTurns--;
+    console.log(this.name+" is no longer buffed!")
+    this.def=Math.round(this.def/1.5);
+    this.buff=null;
+  }
+  if (this.deBTurns>1){
+    this.deBTurns--;
+    console.log(this.name +" is fast asleep!")
+    enemy.battle(this);
+  }else if (this.deBTurns===1){
+    this.deBTurns--;
+    console.log(this.name +" woke up!");
+    this.deBuff=null;
+  }
+
+  this.critCheck(enemy);
 }
 Character.prototype.critCheck= function critCheck(enemy){
-  console.log(this.name+'attacked'+enemy.name);
+  console.log(this.name+' attacked '+enemy.name);
   var success=Math.random();
   var crit;
   if (success<(0.03*this.spd)){
@@ -38,7 +69,7 @@ Character.prototype.bam= function bam(crit,enemy){
 };
 Character.prototype.dodge = function dodge(dmg, enemy, crit) {
   var success=Math.random();
-  if (success>(0.03*enemy.spd)){
+  if (success>(0.03*(enemy.spd))){
     this.dmgDealt(dmg, enemy,crit,false);
   }else{
     this.dmgDealt(0,enemy,crit,true);
@@ -47,29 +78,28 @@ Character.prototype.dodge = function dodge(dmg, enemy, crit) {
 
 Character.prototype.dmgDealt= function dmgDealt(dmg, enemy, crit, dodge){
   if (dodge){
-    console.log(enemy.name+'dodged the attack!')
+    console.log(enemy.name+' dodged the attack!')
     //dodge
   }else if (dmg<=0){
       if (crit){
         console.log('It\'s a critical hit!');
         enemy.hp--;
-        console.log(enemy.name + 'lost 1 health');
+        console.log(enemy.name + ' lost 1 health');
       }else{
         dmg=Math.floor(Math.random()*2);
         enemy.hp-=dmg;
         if (dmg===1){
-          console.log(enemy.name+ 'lost 1 health');
+          console.log(enemy.name+ ' lost 1 health');
         }else{
           console.log('but it did no damage!');
         }
       }
 
   }else {
-      console.log(enemy.name+ "lost "+dmg+" health");
+      console.log(enemy.name+ " lost "+dmg+" health");
       enemy.hp-=dmg;
     }
-  }
-  this.turnEnd();
+  this.turnEnd(enemy);
 
 };
 Character.prototype.turnEnd= function turnEnd(enemy){
@@ -81,36 +111,41 @@ Character.prototype.turnEnd= function turnEnd(enemy){
     }
   }else{
     if (this.player){
-        enemy.critCheck(this);
+        enemy.battle(this);
     }else{
       //wait for selection
     }
   }
 };
 Character.prototype.magic= function magic(enemy){
-
+    //populate menu via this.abilities[i]
+    //call abilities when clicked via girl.abilities[i].spell(this,enemy)
 };
 
 Character.prototype.flee= function flee(enemy){
-  console.log (this.name+'tried to runaway')
+  console.log (this.name+' tried to runaway')
   var success=Math.random();
   if ((this.lvl/enemy.lvl)>success){
-    console.log(this.name+'got away safely!')
+    console.log(this.name+' got away safely!')
     //back to overhead screen
   }
   else{
-    console.log(this.name+'failed to get away');
+    console.log(this.name+' failed to get away');
     enemy.critCheck(this);
   }
 };
 
 Character.prototype.win= function win(enemy){
-  console.log(this.name+"won the battle!")
+  console.log(this.name+" won the battle!");
+  this.deBuff=null;
+  this.buff=null;
+  this.turn=0;
+  this.buffTurns=0;
+  this.deBTurns=0;
   if (this.lvl<10){
     var gain=Math.floor((enemy.lvl*5)/2)
-    console.log(this.name+"gained"+gain+"experience points")
+    console.log(this.name+" gained "+gain+" experience points")
     this.exp+=gain;
-
     if (this.exp===this.expMax){
       this.lvlUp();
     }else{
@@ -123,14 +158,20 @@ Character.prototype.win= function win(enemy){
 
 Character.prototype.lvlUp=function lvlUp(){
   if (this.lvl<10){
-    console.log(this.name+"leveled up!")
+    console.log(this.name+" leveled up!")
     this.lvl+=1;
   }
-  this.ptSelect();
   this.exp=0;
+  this.expMax=lvl*10;
+  if (this.lvl===2){this.abilities.push({name: 'hurt', spell:hurt})}
+  if (this.lvl===4){this.abilities.push({name: 'heal', spell:heal})}
+  if (this.lvl===6){this.abilities.push({name: 'fortyWinks', spell:fortyWinks})}
+  if (this.lvl===8){this.abilities.push({name: 'beBrave', spell:beBrave})}
+  if (this.lvl===10){this.abilities.push({name: 'tantrum', spell:tantrum})}
+  //this.ptSelect();
 };
 
-var player={lvl:1,att:2,def:50,imn:2,spd:1,play: true};
-var enemy={lvl:2,att:2,def:2,imn:2,spd:33};
+var player={name:'sophia',lvl:1,att:2,def:50,imn:2,spd:4,play: true};
+var enemy={name:'fear',lvl:2,att:2,def:2,imn:2,spd:2};
 var girl = new Character(player);
 var fear = new Character(enemy);
